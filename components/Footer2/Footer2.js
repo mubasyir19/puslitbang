50fd5ceae9409a5e425a5f31473cf8cb8431b9e7
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-10-13 23:54:07 +0700

    update(footer2): update padding footer, update text style menu

--- components/Footer2/Footer2.js
+++ components/Footer2/Footer2.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Footer2() {
   return (
-    <footer className='px-10 lg:px-36 pt-16 pb-4 mt-10 bg-background text-white'>
+    <footer className='px-10 lg:px-16 pt-16 pb-4 mt-10 bg-background text-white'>
       <div className='flex flex-col lg:flex-row justify-between'>
         <div>
           <Image src='/images/logo-bmkg.png' width={85} height={85} className='w-auto h-20' alt='logo' />
@@ -13,23 +13,31 @@ export default function Footer2() {
             <p>Klimatologi Geofisika</p>
           </div>
         </div>
-        <div className='mt-10 lg:mt-0 flex flex-col lg:flex-row gap-y-8 gap-x-16'>
+        <div className='mt-10 pe-40 lg:mt-0 flex flex-col lg:flex-row gap-y-8 gap-x-28'>
           <div>
-            <h3 className='text-base underline leading-4 font-medium'>Meteorologi</h3>
+            <h3 className='text-base leading-4 font-bold'>Meteorologi</h3>
             <div className='mt-8 flex flex-col gap-y-6 text-sm'>
-              <Link href='#'>InaNWP</Link>
+              <Link href='#' className='text-gray-500 hover:text-white'>
+                InaNWP
+              </Link>
             </div>
           </div>
           <div>
-            <h3 className='text-base underline leading-4 font-medium'>Klimatologi</h3>
+            <h3 className='text-base leading-4 font-bold'>Klimatologi</h3>
             <div className='mt-8 flex flex-col gap-y-6 text-sm'>
-              <Link href='#'>InaRWC</Link>
-              <Link href='#'>InaSHO</Link>
-              <Link href='#'>InaAQM</Link>
+              <Link href='#' className='text-gray-500 hover:text-white'>
+                InaRWC
+              </Link>
+              <Link href='#' className='text-gray-500 hover:text-white'>
+                InaSHO
+              </Link>
+              <Link href='#' className='text-gray-500 hover:text-white'>
+                InaAQM
+              </Link>
             </div>
           </div>
           <div>
-            <h3 className='text-base underline leading-4 font-medium'>Geofisika</h3>
+            <h3 className='text-base leading-4 font-bold'>Geofisika</h3>
             <div className='mt-8 flex flex-col gap-y-6 text-sm'></div>
           </div>
         </div>
