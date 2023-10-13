7da1a84e889fd119015f99f4efa5f0d94b0e8455
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-10-13 23:45:16 +0700

    update(scope): change font size

--- components/Scope/Scope.js
+++ components/Scope/Scope.js
@@ -17,7 +17,7 @@ export default function Scope() {
           <p className='text-lg font-bold'>Ruang Lingkup</p>
         </div>
         <div className='px-28 w-3/4'>
-          <h1 className='mt-8 text-5xl font-bold'>
+          <h1 className='mt-8 text-3xl font-bold'>
             Kami Menyajikan Informasi Terkait Meteorologi, Klimatologi, dan Geofisika
           </h1>
         </div>
@@ -25,8 +25,8 @@ export default function Scope() {
       <div className='mt-16 flex gap-x-20'>
         <div className='card bg-image-meteo bg-cover w-[427px] h-[421px] rounded-3xl'>
           <div className='content flex flex-col justify-center items-start text-white px-5 bg-black bg-opacity-50 w-full h-full rounded-3xl'>
-            <h3 className='text-4xl font-bold'>Meteorologi</h3>
-            <p className='text-xl mt-5'>
+            <h3 className='text-2xl font-bold'>Meteorologi</h3>
+            <p className='text-base mt-5'>
               Kami memberikan info secara tepat dan akurat terkait cuaca di tiap harinya dengan data yang akurat dan
               juga real time.
             </p>
@@ -37,8 +37,8 @@ export default function Scope() {
         </div>
         <div className='card bg-image-climate bg-cover w-[427px] h-[421px] rounded-3xl'>
           <div className='content flex flex-col justify-center items-start text-white px-5 bg-black bg-opacity-50 w-full h-full rounded-3xl'>
-            <h3 className='text-4xl font-bold'>Klimatologi</h3>
-            <p className='text-xl mt-5'>
+            <h3 className='text-2xl font-bold'>Klimatologi</h3>
+            <p className='text-base mt-5'>
               Kami memberikan info secara tepat dan akurat terkait cuaca di tiap harinya dengan data yang akurat dan
               juga real time.
             </p>
@@ -49,8 +49,8 @@ export default function Scope() {
         </div>
         <div className='card bg-image-geo bg-cover w-[427px] h-[421px] rounded-3xl'>
           <div className='content flex flex-col justify-center items-start text-white px-5 bg-black bg-opacity-50 w-full h-full rounded-3xl'>
-            <h3 className='text-4xl font-bold'>Geofisika</h3>
-            <p className='text-xl mt-5'>
+            <h3 className='text-2xl font-bold'>Geofisika</h3>
+            <p className='text-base mt-5'>
               Kami memberikan info secara tepat dan akurat terkait cuaca di tiap harinya dengan data yang akurat dan
               juga real time.
             </p>
