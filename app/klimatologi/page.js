6a91078bf2092242b53bca96c9118321cccf2eb5
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-10-14 00:02:52 +0700

    add(climate): add header component

--- app/klimatologi/page.js
+++ app/klimatologi/page.js
@@ -1,10 +1,11 @@
-import { Footer, Footer2, Navbar, Warning } from '@/components';
+import { Footer, Footer2, Header, Navbar, Warning } from '@/components';
 import React from 'react';
 
 export default function Klimatologi() {
   return (
     <main className='relative'>
       <Navbar />
+      <Header />
       <Warning />
       <Footer2 />
     </main>
